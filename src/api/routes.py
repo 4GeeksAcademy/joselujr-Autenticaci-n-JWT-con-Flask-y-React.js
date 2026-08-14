180125a911ade4fcc46410ebbb0bef7fca1bba6c
Jose Rodriguez <masullo.joseluis@gmail.com>
2026-08-14 14:52:08 +0000

    rutas y modelos ok

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +22,57 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+@api.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"error": "Email y contraseña son obligatorios"}), 400
+
+    existing_user = db.session.execute(db.select(User).where(
+        User.email == email)).scalar_one_or_none()
+    if existing_user:
+        return jsonify({"error":"Usuario ya existe"}),400
+
+    new_user = User(email=email)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg":"Usuario creado correctamente"}),201
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"error": "Email y contraseña son obligatorios"}), 400
+
+    existing_user = db.session.execute(db.select(User).where(
+            User.email == email)).scalar_one_or_none()
+
+    if existing_user is None:
+        return jsonify({"error":"email y/o contraseña invalido"}), 401
+
+    if existing_user.check_password(password):
+        acces_token = create_access_token(identity=str(existing_user.id))
+        return jsonify({"msg":"El login ha sido correcto", "token": acces_token})
+    else: return jsonify({"error": "email y/o contraseña invalido"}), 401
+
+
+@api.route("/profile", methods=["GET"])
+@jwt_required()
+def get_profile():
+    user_id = get_jwt_identity()
+    user = db.session.get(User, user_id)
+    if not user:
+        return jsonify({"error":"Usuario no encontrado"}), 404
+
+    return jsonify(user.serialize()), 200
